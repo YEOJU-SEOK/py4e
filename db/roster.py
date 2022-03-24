a2ef01f75c49a966f8494c81140bb0ffd32f0d84
YEOJU-SEOK <yeoju.ac@gmail.com>
2022-03-24 19:41:16 +0900

    sqlite

--- db/roster.py
+++ db/roster.py
@@ -31,7 +31,7 @@ CREATE TABLE Member (
 fname = input('enter file name :')
 
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
 str_data = open(fname).read()
 json_data = json.loads(str_data)
